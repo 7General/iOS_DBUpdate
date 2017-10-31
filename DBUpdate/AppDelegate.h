64f5490dfad2a07b0e1a9e736935204e1c61f44c
王会洲 <wanghuizhou21@163.com>
2017-10-31 10:39:11 +0800

    上传基本工程

---
+++ DBUpdate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DBUpdate
+//
+//  Created by ZZG on 17/10/31.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
